c4060e2e7a4fa305b61d6773f8818878eef9f244
louisoduor <louisodhiambo20@gmail.com>
2023-12-12 08:40:32 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42); 
+  }
+  
+
+
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264; 
+  }
+
+
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTraveled = Math.abs(destination - start);
+    return blocksTraveled * 264; 
+  }
+
+
+  function calculatesFarePrice(start, destination) {
+    const feetTraveled = distanceTravelledInFeet(start, destination);
+  
+    if (feetTraveled <= 400) {
+      return 0; 
+    } else if (feetTraveled > 400 && feetTraveled <= 2000) {
+      return (feetTraveled - 400) * 0.02; 
+    } else if (feetTraveled > 2000 && feetTraveled <= 2500) {
+      return 25; 
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
